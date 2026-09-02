1850102245378a53683e8aa98977d0d14571c17a
ariankhedmatgozar <ariankhedmatgozar@gmail.com>
2026-09-02 12:36:41 +0330

    added file type security

--- uploads/upload_process.php
+++ uploads/upload_process.php
@@ -13,6 +13,52 @@ if (!isset($_FILES["file"])) {
 
 $file = $_FILES["file"];
 
+if ($file["error"] !== UPLOAD_ERR_OK) {
+    exit("File upload error.");
+}
+
+$maxFileSize = 2 * 1024 * 1024;
+
+if ($file["size"] > $maxFileSize) { 
+    exit("File is too large."); 
+}
+
+
+$allowedExtensions = [
+    "jpg",
+    "jpeg",
+    "png",
+    "gif",
+    "pdf",
+    "txt"
+];
+
+$fileName = basename($file["name"]);
+
+$extension = strtolower(
+    pathinfo($fileName, PATHINFO_EXTENSION)
+);
+
+if (!in_array($extension, $allowedExtensions, true)) {
+    exit("File type not allowed.");
+}
+
+$finfo = new finfo(FILEINFO_MIME_TYPE);
+
+$mimeType = $finfo->file($file["tmp_name"]);
+
+$allowedMimeTypes = [
+    "image/jpeg",
+    "image/png",
+    "image/gif",
+    "application/pdf",
+    "text/plain"
+];
+
+if (!in_array($mimeType, $allowedMimeTypes, true)) {
+    exit("Invalid file type.");
+}
+
 $uploadDir = __DIR__ . "/" ;
 
 
